79a22f1a3f73b6df065a29ae953a902dc856a25a
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2022-10-30 14:20:44 +0400

    correct some mistakes

--- script.js
+++ script.js
@@ -4,31 +4,26 @@ let data = [
     {
         id: 1,
         imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/289e90b8-fa1c-4fb9-b90a-6758a016d650.jpeg',
-        title: 'Georgian Healthy products',
         url: 'https://www.google.com/search?q=flamingo&sxsrf=ALiCzsYljkbPSNkNuKNn37zWGfgz4Aew9g:1652188105534&source=lnms&tbm=isch&sa=X&ved=2ahUKEwito4epgNX3AhVSSvEDHXIiB2sQ_AUoAXoECAMQAw&biw=1920&bih=969&dpr=1',
     },
     {
         id: 2,
         imageUrl: 'https://thumbs.dreamstime.com/z/traditional-georgian-sweets-called-churchkhela-churchela-candle-shaped-grape-juice-candy-nuts-inside-traditional-georgian-161801425.jpg',
-        title: 'Georgian Healthy products',
         url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0lwHzvnxdwvb7S9D4wHAlqDBJoxRRQZ5re5Et0xmLnExan2nhtXTn9CUxJzF9Wp_RtG0&usqp=CAU'
     },
     {
         id: 3,
         imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/65c4b4ec-6829-4d24-8c98-9171219e8ab8.jpeg',
-        title: 'Georgian Healthy products',
         url: 'https://www.google.com/search?q=elephant&tbm=isch&ved=2ahUKEwig85LVgNX3AhUZgv0HHcikCMIQ2-cCegQIABAA&oq=elephant&gs_lcp=CgNpbWcQAzIHCCMQ7wMQJzIHCCMQ7wMQJzIFCAAQgAQyBAgAEEMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgARQ2A9YuRRg4h1oAHAAeACAAZ4CiAGIBZIBBTAuMi4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=JWR6YuDMPJmE9u8PyMmikAw&bih=969&biw=1920'
     },
     {
         id: 4,
         imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO3qVvhp-R6h7fon6se5p53lJNXTNrnVZyTIfUth8xhSirtinFVoypfTP11noeF29E3Tc&usqp=CAU',
-        title: 'Georgian Healthy products',
         url: 'https://www.google.com/search?q=crowned+crane&hl=en&sxsrf=ALiCzsZnbFDyz6pgK7rb45KuI7I_g2f1lQ:1652188368850&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjU4M6mgdX3AhVbQvEDHYQHDTkQ_AUoAXoECAIQAw&biw=1920&bih=969&dpr=1'
     },
     {
         id: 5,
         imageUrl: 'https://www.dolceconfections.com/wp-content/uploads/2020/03/Mixed-Dreid-Fruit.jpg',
-        title: 'Georgian Healthy products',
         url: 'https://www.google.com/search?q=Mandarin+duck&sxsrf=ALiCzsYalOgGWVkNRzipbE5oN3aQ0EFoiA:1652188480823&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiwi4HcgdX3AhWvQvEDHSgxDv0Q_AUoAXoECAIQAw&biw=1920&bih=969&dpr=1'
     },
 ];
@@ -49,7 +44,6 @@ function createATag (item) {
 }
 
 function createImgTag(item) {
-    // let imgTag = document.body.style.backgroundImage = 'url(' + item.imageUrl + ')';
     sliderContainer.style.backgroundImage = 'url('+ item.imageUrl +')';
     sliderContainer.style.backgroundRepeat = "no-repeat";
     sliderContainer.style.backgroundSize = "cover";
@@ -61,7 +55,6 @@ function createH2Tag (item) {
     h2Tag.setAttribute.href = item.url;
     h2Tag.classList.add('slider-title');
     h2Tag.append(item.title);
-
     return h2Tag;
 }
 
